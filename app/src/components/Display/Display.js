56e79917ac2ab58f1efb32744922afb9cc39ae5a
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-09 18:25:40 -0400

    More Test to Display when props change

--- app/src/components/Display/Display.js
+++ app/src/components/Display/Display.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Display = (props) => {
     return (
         <>
-            <h1>Balls: {props.balls ? props.balls : 0} | Strikes: {props.strikes ? props.strikes : 0}</h1>
+            {props.balls && props.strikes ? 
+                <h1>Balls: <span data-testid="balls-count">{props.balls}</span> | Strikes: <span data-testid="strikes-count">{props.strikes}</span></h1>
+            :
+                <h1>Balls: 0 | Strikes: 0</h1>
+            }
         </>
     )
 }
--- app/src/components/Display/Display.test.js
+++ app/src/components/Display/Display.test.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import {render} from 'react-testing-library'
+import {render, cleanup} from 'react-testing-library'
 import 'jest-dom/extend-expect'
 
 import Display from './Display'
 
+
 describe('<Display/>', () => {
-	it('renders `Balls: 0 | Strikes: 0` if no balls and strikes props provided', () => {
+	it('renders `Balls:  | Strikes: ` if no balls and strikes props provided', () => {
         const {queryByText} = render(<Display/>)
         expect(queryByText(/Balls: 0 | Strikes: 0 /i)).not.toBeNull()
     });
-
+    afterEach(cleanup)
     it('renders `Balls: props.balls | Strikes: props.strikes` if balls and strikes props provided', () => {
-        const {queryByText} = render(<Display balls = {3} strikes = {1}/>)
-        expect(queryByText(/Balls: 3 | Strikes: 1 /i)).not.toBeNull()
+        const {getByTestId, queryByText, rerender} = render(<Display balls = {0} strikes = {0}/>)
+        expect(queryByText(/Balls: 0 | Strikes: 0 /i)).not.toBeNull()
+
+        rerender(<Display balls = {1} strikes = {1}/>)
+        expect(getByTestId('balls-count').textContent).toBe('1')
+        expect(getByTestId('strikes-count').textContent).toBe('1')
+
+        rerender(<Display balls = {3} strikes = {2}/>)
+        expect(getByTestId('balls-count').textContent).toBe('3')
+        expect(getByTestId('strikes-count').textContent).toBe('2')
     });
 })
